cba07b47fe368735983a5d60711853b818a58698
Sunil Kumar <sunil.kumar@nescode.com>
2016-02-23 11:41:17 +0530

    Readme added

--- sellers/models.py
+++ sellers/models.py
@@ -5,7 +5,11 @@ from django.db import models
 
 class SellerAccount(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    managers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="manager_sellers", blank=True)
+    managers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="manager_sellers",
+        blank=True
+    )
     active = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
