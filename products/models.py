112132596df74289a2feea6501c27ec47db6f3d1
Sunil Kumar <sunil.kumar@nescode.com>
2016-02-25 18:42:06 +0530

    Ajax purchase then download

--- products/models.py
+++ products/models.py
@@ -33,6 +33,7 @@ class Product(models.Model):
         view_name = "products:detail_slug"
         return reverse(view_name, kwargs={"slug": self.slug})
 
+    
     def get_download(self):
         view_name = "products:download_slug"
         url = reverse(view_name, kwargs={"slug": self.slug})
