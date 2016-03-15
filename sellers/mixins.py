45796ad0ed2225455eb5de836efb222b9acd972c
Sunil Kumar <sunil.kumar@nescode.com>
2016-03-15 09:02:13 +0530

    Seller account mixins

---
+++ sellers/mixins.py
@@ -0,0 +1,31 @@
+from billing.models import Transaction
+from djangodm.mixins import LoginRequiredMixin
+from products.models import Product
+
+from .models import SellerAccount
+
+
+
+class SellerAccountMixin(LoginRequiredMixin, object):
+	account = None
+	products = []
+	transactions = []
+
+	def get_account(self):
+		user = self.request.user
+		accounts = SellerAccount.objects.filter(user=user)
+		if accounts.exists() and accounts.count() == 1:
+			self.account = accounts.first()
+			return accounts.first()
+		return None
+
+	def get_products(self):
+		account = self.get_account()
+		products = Product.objects.filter(seller=account)
+		self.products = products
+		return products
+
+	def get_transactions(self):
+		products = self.get_products()
+		transactions = Transaction.objects.filter(product__in=products)
+		return transactions
--- sellers/views.py
+++ sellers/views.py
@@ -2,71 +2,62 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.views.generic.edit import FormMixin
 from django.views.generic.list import ListView
+# Create your views here.
 
 from billing.models import Transaction
 from djangodm.mixins import LoginRequiredMixin
 from products.models import Product
 
 from .forms import NewSellerForm
+from .mixins import SellerAccountMixin
 from .models import SellerAccount
 
-class SellerTransactionListView(ListView):
-    model = Transaction
-    template_name = "Sellers/transaction_list_view.html"
 
-    def get_queryset(self):
-        account = SellerAccount.objects.filter(user=self.request.user)
-        if account.exists():
-            products = Product.objects.filter(seller=account)
-            return Transaction.objects.filter(product__in=products)
-        return []
+class SellerTransactionListView(SellerAccountMixin, ListView):
+	model = Transaction
+	template_name = "sellers/transaction_list_view.html"
+
+	def get_queryset(self):
+		return self.get_transactions()
 
 
-class SellerDashboard(LoginRequiredMixin, FormMixin, View):
-    form_class = NewSellerForm
-    success_url = "/sellers/"
 
-    def post(self, request, *args, **kwargs):
-        form = self.get_form()
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
 
-    def get(self, request, *args, **kwargs):
-        apply_form = self.get_form()
-        account = SellerAccount.objects.filter(user=self.request.user)
-        exists = account.exists()
-        active = None
+class SellerDashboard(SellerAccountMixin, FormMixin, View):
+	form_class = NewSellerForm
+	success_url = "/seller/"
 
-        context = {
-            "apply_form":apply_form,
-            "account": account,
-            "active": active,
-            "exists": exists,
-        }
-        context = {}
+	def post(self, request, *args, **kwargs):
+		form = self.get_form()
+		if form.is_valid():
+			return self.form_valid(form)
+		else:
+			return self.form_invalid(form)
 
-        if exists:
-            account = account.first()
-            active = account.active
+	def get(self, request, *args, **kwargs):
+		apply_form = self.get_form() #NewSellerForm()
+		account = self.get_account()
+		exists = account
+		active = None
+		context = {}
+		if exists:
+			active = account.active
+		if not exists and not active:
+			context["title"] = "Apply for Account"
+			context["apply_form"] = apply_form
+		elif exists and not active:
+			context["title"] = "Account Pending"
+		elif exists and active:
+			context["title"] = "Seller Dashboard"
+			#products = Product.objects.filter(seller=account)
+			context["products"] = self.get_products()
+			context["transactions"] = self.get_transactions()[:5]
+		else:
+			pass
 
-        if not exists and not active:
-            context["title"] = "Apply for Account"
-            context["apply_form"] = apply_form
-        elif exists and not active:
-            context["title"] = "Accont Pending"
-        elif exists and active:
-            context["title"] = "Seller Dashboard"
-            products = Product.objects.filter(seller=account)
-            context["products"] = products
-            context["transactions"] = Transaction.objects.filter(product__in=products)[:6]
-        else:
-            pass
+		return render(request, "sellers/dashboard.html", context)
 
-        return render(request, "sellers/dashboard.html", context)
-
-        def form_valid(self, form):
-            valid_data = super(SellerDashboard, self).form_valid(form)
-            obj = SellerAccount.objects.create(user=self.request.user)
-            return valid_data
+	def form_valid(self, form):
+		valid_data = super(SellerDashboard, self).form_valid(form)
+		obj = SellerAccount.objects.create(user=self.request.user)
+		return valid_data
