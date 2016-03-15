f0035afc9bf53331f2755fe8d8d24fa694ac1b09
Sunil Kumar <sunil.kumar@nescode.com>
2016-03-15 18:43:46 +0530

    Seller edit product

--- products/mixins.py
+++ products/mixins.py
@@ -1,22 +1,17 @@
 from django.http import Http404
 
 from djangodm.mixins import LoginRequiredMixin
+from sellers.mixins import SellerAccountMixin
 
-class ProductManagerMixin(LoginRequiredMixin, object):
+class ProductManagerMixin(SellerAccountMixin, object):
     def get_object(self, *args, **kwargs):
-        user = self.request.user
+        seller = self.get_account()
         obj = super(ProductManagerMixin, self).get_object(*args, **kwargs)
         try:
-            obj.user == user
+            obj.seller == seller
         except:
             raise Http404
-
-        try:
-            user in obj.managers.all()
-        except:
-            raise Http404
-
-        if obj.user == user or user in obj.managers.all():
+        if obj.seller == seller:
             return obj
         else:
             raise Http404
